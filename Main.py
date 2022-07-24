e66302e6cb2348b74fa71e363f52857933a591f4
Matthew Bowers <bowers.matthew9@gmail.com>
2022-07-24 13:43:58 +1200

    Union of the lexeme and manual input

--- Main.py
+++ Main.py
@@ -6,9 +6,19 @@ from pattern.text.en import lexeme
 
 word_list = []
 graph = Graph.Graph()
+lexeme_dictionary = {}
 
 if Config.USE_TEST_DATA:
     word_list = ["Bamboozled", "Risk", "Shipping", "Packaging"]
+    # For each key word. Finding the Lexeme of each.
+    # Lexeme is a unit of lexical meaning that underlies a set of words that are related through inflection
+    # So if the key word was "ship" then the lexeme set would be ["shipping", "shipped"]
+    for word in word_list:
+        # Unfortunately, lexeme() will throw an error if no inflections exist for the word.
+        try:
+            lexeme_dictionary[word] = lexeme(word)
+        except RuntimeError as e:
+            lexeme_dictionary[word] = []
 else:
     # TODO: have file in teams and use webhook?
     word_list = HelperFunctions.read_keywords(Config.PATH_TO_WORD_LIST, Config.WORD_LIST_SHEET_NAME)
@@ -16,27 +26,25 @@ else:
         # Printing the first 10 rows of the df
         print("Word list:")
         print(word_list[:10])
+    # For each key word. Finding the Lexeme of each.
+    # Lexeme is a unit of lexical meaning that underlies a set of words that are related through inflection
+    # So if the key word was "ship" then the lexeme set would be ["shipping", "shipped"]
+    for row in word_list.iterrows():
+        # row[0] is the row index, row[1] is the row data
+        inflections = []
+        words = row[1]
+        word = words[0]
+        for w in words:
+            if w != "" and type(w) is not float:
+                inflections.append(w)
+        # trying to get the lexeme set for the word
+        try:
+            lexeme_set = lexeme(word)
+        except RuntimeError as e:
+            lexeme_set = []
+        # Getting the union of the two lists.
+        lexeme_dictionary[word] = list(set(lexeme_set) | set(inflections))
 
-# For each key word. Finding the Lexeme of each.
-# Lexeme is a unit of lexical meaning that underlies a set of words that are related through inflection
-# So if the key word was "ship" then the lexeme set would be ["shipping", "shipped"]
-lexeme_dictionary = {}
-for word in word_list.iterrows():
-    print(word[1][0])
-    # lexeme has no handling for if the word doesn't have any inflections.
-    # For instance, Bamboozled will throw a RuntimeError.
-    try:
-        if Config.USE_TEST_DATA:
-            lexeme_dictionary[word] = lexeme(word)
-        else:
-            lexeme_dictionary[word] = list(lexeme(word[0]) | word[1:])
-        if Config.DEBUG:
-            print(f"Lexeme for word {word}:")
-            # The call time is small enough that this is insignificant to the runtime
-            # It's ok just for debugging
-            print(lexeme(word))
-    except RuntimeError as e:
-        lexeme_dictionary[word] = []
 
 # Looping through the words
 for key in lexeme_dictionary:
