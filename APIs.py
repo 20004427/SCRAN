f8f7729c1f85dee39d814d8a4c688650e3a72170
Matthew Bowers <bowers.matthew9@gmail.com>
2022-07-24 16:56:44 +1200

    started work on google scrape

--- APIs.py
+++ APIs.py
@@ -1,7 +1,10 @@
 import requests
+import urllib
 import json
 from pattern.text.en import lexeme
 from io import BytesIO
+from requests_html import HTML
+from requests_html import HTMLSession
 
 import Config
 
@@ -42,4 +45,41 @@ def make_call_to_dictionary(word):
         return response.json()[0]
     except KeyError as e:
         print(f"[WARNING] Definition not found for {word}")
+        return -1
+
+# __________ GOOGLE __________
+
+
+def scrape_google(word):
+    """
+    Function to scrape google results.
+    Looks at the top 10 sites, and looks for keywords.
+    Returns a list of keywords.
+    Note that the input does not need to contain 'supply chain',
+    This function adds 'Supply chain' to the start of the query.
+    This isn't technically an API, but thought that it should probably be here.
+    TODO: Move this function to a different location?
+        See issue #5: https://github.com/20004427/SCRAN/issues/5
+
+    :param word: (String) Ideally a word, but can be a phrase
+    :return: (Array| String) List of keywords relating to the input
+    """
+    query = urllib.parse.quote_plus("Supply chain " + word)
+    response = get_source(f"https://www.google.co.nz/search?q={query}")
+    links = list(response.html.absolute_links)
+
+
+def get_source(url):
+    """Returns the source code for the provided URL.
+
+    :param url: (String) Url of the page to scrape
+    :return: (object | requests_html)
+    """
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        return response
+    except requests.exceptions.RequestException as e:
+        if Config.DEBUG:
+            print(f"[ERROR] {e.strerror()}")
         return -1
