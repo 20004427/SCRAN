718b2b0aa28df24d4d07f2d0307cf6f136ee95cd
Matthew Bowers <bowers.matthew9@gmail.com>
2022-08-16 21:03:12 +1200

    Update Main.py

--- Main.py
+++ Main.py
@@ -62,6 +62,8 @@ for key in lexeme_dictionary:
         graph.add_edge(key, word)
     if Config.DEBUG:
         print(f"The keywords relating to {key} are {linking_keywords}")
-nx.draw(graph, with_labels=True)
+# Output
+pos = nx.spring_layout(graph)
+nx.draw(graph, with_labels=True, pos=pos)
 pt.savefig("output.png")
 HelperFunctions.export_to_pajek(graph)
